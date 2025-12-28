d3170a19203930dd1d0baa83c98478d713501f06
Mike McDonald <mikemcdonald83@gmail.com>
2025-12-27 20:26:44 -0600

    Reverted and updated link to static route.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <p>Welcome to my GitHub App PKCE testing scratch.</p>
-<a href="/admin/index.html">Go to Sveltia Admin</a>
+<a href="{base}/admin/index.html">Go to Sveltia Admin</a>
